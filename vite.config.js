6b028ad152f921dca4d83fb4f45bb49395cceaa1
Darya Sevostyanova <sevostyanova2016@icloud.com>
2025-10-20 16:20:01 +0300

    add sass-embedded
    add preprocessorOptions
    
    update vite config
    
    try to fix deploy
    
    try to fix code after deploy
    
    break build
    
    break build

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,24 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-    root: 'src',  // Корень проекта — src
+    root: 'src',
     server: {
-        open: '/index.html'  // Открывать index.html при запуске dev-сервера
+        open: '/index.html'
     },
     build: {
-        outDir: '../dist',  // Вывод сборки в ../dist (корневая dist)
+        outDir: path.resolve(__dirname, 'dist'),
         rollupOptions: {
             input: {
-                main: path.resolve(__dirname, 'src/index.html'),  // Основная страница
-                channel: path.resolve(__dirname, 'src/channelPage.html')  // Дополнительная страница
-                // Добавьте здесь другие HTML, если появятся, например:
-                // another: path.resolve(__dirname, 'src/anotherPage.html')
+                main: path.resolve(__dirname, 'src/index.html'),
+                channel: path.resolve(__dirname, 'src/channelPage.html')
             }
         }
     },
-    // publicDir: '../public'  // Закомментируйте или удалите, если public не используется
-    // Или установите publicDir: false, если нет статических ассетов вне src
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // additionalData: `@import "src/styles/_variables.scss";`,
+            }
+        }
+    }
 })
